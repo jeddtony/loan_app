9d96400bd8e61e256e2d5d9f52d1ec81da2d2c26
jeddtony <jedtony.jt@gmail.com>
2019-07-18 12:54:46 +0100

    Remove id each object

--- database/seed/loanFactory.js
+++ database/seed/loanFactory.js
@@ -1,6 +1,5 @@
 db.loans.insert([
     {
-      "_id": "5d2cf38099024319e38d2bb0",
       "name": "Bridget Blackwell",
       "user_id": 18,
       "rate": 18,
@@ -8,7 +7,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf3808f6fc9948b35ab3c",
       "name": "Landry Cooke",
       "user_id": 4,
       "rate": 16,
@@ -16,7 +14,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf380d9f9df0803e43764",
       "name": "Cathleen Contreras",
       "user_id": 12,
       "rate": 29,
@@ -24,7 +21,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf380c4675b318d113e37",
       "name": "Mindy Anderson",
       "user_id": 2,
       "rate": 17,
@@ -32,7 +28,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf380b6f8d371ea7f95b6",
       "name": "Audra Massey",
       "user_id": 18,
       "rate": 17,
@@ -40,7 +35,6 @@ db.loans.insert([
       "status": 1
     },
     {
-      "_id": "5d2cf380e50dd4b937d0bdb8",
       "name": "Tameka Kidd",
       "user_id": 14,
       "rate": 15,
@@ -48,7 +42,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf380c4f343c58bb119e6",
       "name": "Farley Rosales",
       "user_id": 3,
       "rate": 16,
@@ -56,7 +49,6 @@ db.loans.insert([
       "status": 1
     },
     {
-      "_id": "5d2cf380ae08d5db2f13cb84",
       "name": "Walsh Curtis",
       "user_id": 11,
       "rate": 28,
@@ -64,7 +56,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf38098601fe7426fea50",
       "name": "Tamera Riddle",
       "user_id": 19,
       "rate": 29,
@@ -72,7 +63,6 @@ db.loans.insert([
       "status": 1
     },
     {
-      "_id": "5d2cf3800fddfbdf20e95c7c",
       "name": "Matthews Vega",
       "user_id": 5,
       "rate": 15,
@@ -80,7 +70,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf3807df9fb8d98bcbbb0",
       "name": "Holly Mullen",
       "user_id": 9,
       "rate": 24,
@@ -88,7 +77,6 @@ db.loans.insert([
       "status": 1
     },
     {
-      "_id": "5d2cf380bdc6e666c24f0d78",
       "name": "Angela Miranda",
       "user_id": 8,
       "rate": 28,
@@ -96,7 +84,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf3804a598a1bc2bdcc8d",
       "name": "Yang Bird",
       "user_id": 2,
       "rate": 23,
@@ -104,7 +91,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf3800f556ec61d760800",
       "name": "Walters Marshall",
       "user_id": 14,
       "rate": 18,
@@ -112,7 +98,6 @@ db.loans.insert([
       "status": 1
     },
     {
-      "_id": "5d2cf380b42e10c4a2a70917",
       "name": "Mullins Mathews",
       "user_id": 7,
       "rate": 10,
@@ -120,7 +105,6 @@ db.loans.insert([
       "status": 1
     },
     {
-      "_id": "5d2cf380ba36e29767ba113b",
       "name": "Duncan Salinas",
       "user_id": 11,
       "rate": 19,
@@ -128,7 +112,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf380e9c0685193401c08",
       "name": "Myrna Vaughn",
       "user_id": 15,
       "rate": 18,
@@ -136,7 +119,6 @@ db.loans.insert([
       "status": 0
     },
     {
-      "_id": "5d2cf380fbca818cc6e49c7e",
       "name": "Randolph Cohen",
       "user_id": 7,
       "rate": 13,
@@ -144,11 +126,10 @@ db.loans.insert([
       "status": 1
     },
     {
-      "_id": "5d2cf3809e5eca3cb3c640cf",
       "name": "Jordan Robertson",
       "user_id": 11,
       "rate": 21,
       "duration": 5,
       "status": 1
     }
-  ])
+  ]);
