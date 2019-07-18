ee033fd521391b70c0473a79218fa636f6a0a970
jeddtony <jedtony.jt@gmail.com>
2019-07-18 12:55:27 +0100

    Add new feature
    - View each loan by id

--- routes/loanRoute.js
+++ routes/loanRoute.js
@@ -5,9 +5,24 @@ function routes(Loan){
     const loanRouter = express.Router();
     const controller = loanController(Loan);
     loanRouter.route('/loans')
-    .get(controller.get);
+    .get(controller.index);
+
+    loanRouter.use('/loans/:loanId', (req, res, next) => {
+        Loan.findById(req.params.loanId, (err, loan) => {
+            if(err){
+                console.log(`There was an error ${err}`);
+                res.send(err);
+            }
+            if(loan){
+                req.loan = loan;
+                return next();
+            }
+            return res.sendStatus(404);
+        });
+    });
+    loanRouter.route('/loans/:loanId')
+    .get(controller.show);
 
  return loanRouter;   
 }
-
 module.exports = routes;
