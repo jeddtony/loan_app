6602f181e49e6ccd435e37295fc8d9cb107c1fea
Jedidiah Anthony <j.anthony@CONCEPTGROUP-NG.COM>
2019-07-22 18:17:48 +0100

    Add more restrictions to model

--- models/user.js
+++ models/user.js
@@ -1,15 +1,26 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const {Schema} = mongoose;
 
 const userModel = new Schema({
     name: {type: String},
-    email: {type: String},
-    password: {type: String},
+    email: {type: String, unique:true, required:true},
+    password: {type: String, required: true},
     address: {type: String},
     occupation: {type: String},
     office_address: {type: String},
     phone_number: {type: String}
 });
 
+
+
+userModel.methods.comparePassword = function (passw, cb){
+    bcrypt.compare(passw, this.password, (err, isMatch)=>{
+        if(err){
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+}
 module.exports = mongoose.model('User', userModel);
