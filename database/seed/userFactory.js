85f7ca9944f968234c73753b12d50b9f27b0232f
Jedidiah Anthony <j.anthony@CONCEPTGROUP-NG.COM>
2019-07-22 18:17:07 +0100

    Modify user factory

--- database/seed/userFactory.js
+++ database/seed/userFactory.js
@@ -1,6 +1,5 @@
 [
     {
-      "_id": "5d2cf112a601b2bea9a2a22d",
       "name": "Lina Lyons",
       "email": "linalyons@retrotex.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -9,7 +8,6 @@
       "company": "QUADEEBO"
     },
     {
-      "_id": "5d2cf112edc693470c0b4650",
       "name": "Ward Manning",
       "email": "wardmanning@quadeebo.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -18,7 +16,6 @@
       "company": "INRT"
     },
     {
-      "_id": "5d2cf1124d11cfee118b271a",
       "name": "Rosella Mann",
       "email": "rosellamann@inrt.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -27,7 +24,6 @@
       "company": "GEOSTELE"
     },
     {
-      "_id": "5d2cf1122be22d049e09f188",
       "name": "Chelsea Serrano",
       "email": "chelseaserrano@geostele.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -36,7 +32,6 @@
       "company": "EXOTECHNO"
     },
     {
-      "_id": "5d2cf112b54b47fbf580a086",
       "name": "Rhonda Frazier",
       "email": "rhondafrazier@exotechno.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -45,7 +40,6 @@
       "company": "AQUOAVO"
     },
     {
-      "_id": "5d2cf1126cf75d49c89b9a5a",
       "name": "Young Chandler",
       "email": "youngchandler@aquoavo.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -54,7 +48,6 @@
       "company": "XERONK"
     },
     {
-      "_id": "5d2cf112c6903e15180803b4",
       "name": "Candace Curry",
       "email": "candacecurry@xeronk.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -63,7 +56,6 @@
       "company": "PORTICO"
     },
     {
-      "_id": "5d2cf1126d246d76516a1d94",
       "name": "Cornelia Coleman",
       "email": "corneliacoleman@portico.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -72,7 +64,6 @@
       "company": "MAROPTIC"
     },
     {
-      "_id": "5d2cf11283daf8501a713769",
       "name": "Pena Yang",
       "email": "penayang@maroptic.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -81,7 +72,6 @@
       "company": "MANGELICA"
     },
     {
-      "_id": "5d2cf112cd9382f0a2da0df0",
       "name": "Hewitt Hinton",
       "email": "hewitthinton@mangelica.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -90,7 +80,6 @@
       "company": "SCENTRIC"
     },
     {
-      "_id": "5d2cf1120191347f9d47741e",
       "name": "Solis Martin",
       "email": "solismartin@scentric.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -99,7 +88,6 @@
       "company": "FLOTONIC"
     },
     {
-      "_id": "5d2cf112990b73ef6e8f3519",
       "name": "Toni Drake",
       "email": "tonidrake@flotonic.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -108,7 +96,6 @@
       "company": "ARCTIQ"
     },
     {
-      "_id": "5d2cf1125af01b95536e80bd",
       "name": "Kathie Leach",
       "email": "kathieleach@arctiq.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -117,7 +104,6 @@
       "company": "QUONK"
     },
     {
-      "_id": "5d2cf1125b2d67495f4b76fd",
       "name": "Leta Pace",
       "email": "letapace@quonk.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -126,7 +112,6 @@
       "company": "CANDECOR"
     },
     {
-      "_id": "5d2cf11299403bfcc66faea6",
       "name": "Latonya Gibson",
       "email": "latonyagibson@candecor.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
@@ -135,7 +120,6 @@
       "company": "ZYTRAC"
     },
     {
-      "_id": "5d2cf112086a1ddd4a1efeaa",
       "name": "Ella Gallegos",
       "email": "ellagallegos@zytrac.com",
       "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
