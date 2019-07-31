604e4e4c26e52480d31d7bc400df3acfa27a6873
Jedidiah Anthony <j.anthony@CONCEPTGROUP-NG.COM>
2019-07-31 17:04:25 +0100

    Change it to use faker

--- database/seed/loanFactory.js
+++ database/seed/loanFactory.js
@@ -1,92 +1,31 @@
-db.loans.insert([
-  {
-    "repaid": true,
-    "tenor": 12,
-    "status": "pending",
-    "amount": 96202901,
-    "paymentInstallment": 96540.715551,
-    "balance": 47448.2772,
-    "interest": 0.7
-  },
-  {
-    "repaid": false,
-    "tenor": 5,
-    "status": "approved",
-    "amount": 21215919,
-    "paymentInstallment": 95112.009767,
-    "balance": 351949.1166,
-    "interest": 0.7
-  },
-  {
-    "repaid": true,
-    "tenor": 10,
-    "status": "approved",
-    "amount": 57828276,
-    "paymentInstallment": 11218.336573,
-    "balance": 488494.8134,
-    "interest": 0.5
-  },
-  {
-    "repaid": true,
-    "tenor": 2,
-    "status": "approved",
-    "amount": 74442234,
-    "paymentInstallment": 16294.684986,
-    "balance": 461035.6271,
-    "interest": 0.8
-  },
-  {
-    "repaid": false,
-    "tenor": 11,
-    "status": "approved",
-    "amount": 23060037,
-    "paymentInstallment": 15050.401974,
-    "balance": 128746.3263,
-    "interest": 0.8
-  },
-  {
-    "repaid": true,
-    "tenor": 4,
-    "status": "approved",
-    "amount": 84915030,
-    "paymentInstallment": 27330.978638,
-    "balance": 49493.969,
-    "interest": 0.5
-  },
-  {
-    "repaid": true,
-    "tenor": 3,
-    "status": "approved",
-    "amount": 41479291,
-    "paymentInstallment": 45133.027215,
-    "balance": 647692.666,
-    "interest": 0.9
-  },
-  {
-    "repaid": true,
-    "tenor": 5,
-    "status": "pending",
-    "amount": 36137556,
-    "paymentInstallment": 54564.993595,
-    "balance": 89382.522,
-    "interest": 0.6
-  },
-  {
-    "repaid": true,
-    "tenor": 8,
-    "status": "pending",
-    "amount": 56940352,
-    "paymentInstallment": 33097.080082,
-    "balance": 962126.529,
-    "interest": 0.5
-  },
-  {
-    "repaid": true,
-    "tenor": 10,
-    "status": "rejected",
-    "amount": 47713003,
-    "paymentInstallment": 36251.184899,
-    "balance": 362192.9868,
-    "interest": 0.6
+const faker = require('faker');
+const Users = require('../../models/user');
+const util = require('util');
+function loanFactory(Loan){
+  
+  function seed(req, res){
+    const counter = req.params.counter;
+    Array.prototype.random = function(){
+      return this[Math.floor((Math.random()* this.length))];
+    }
+    let allUsers = Users.find({}, 'name email', (err, users) => users);
+    // res.json(allUsers);
+    console.log(allUsers.obj);
+    // let stringifiedUsers = (util.inspect(allUsers));
+    // stringifiedUsers.reduce((userMap, item)=>{
+    //   userMap[item.id] = item;
+    //   return userMap
+    // });
+
+    // console.log(stringifiedUsers);
+
+    // for(let i = 0; i < counter; i++){
+    //   new Loan(
+
+    //   )
+    // }
   }
-  ]);
+  return { seed };
+}
+
+module.exports = loanFactory;
