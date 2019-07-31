511f6a7077c4c3e16224297ebe29fdf0196dbf16
Jedidiah Anthony <j.anthony@CONCEPTGROUP-NG.COM>
2019-07-31 17:04:45 +0100

    Change it to use faker

--- database/seed/userFactory.js
+++ database/seed/userFactory.js
@@ -1,130 +1,34 @@
-[
-    {
-      "name": "Lina Lyons",
-      "email": "linalyons@retrotex.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "670 Monument Walk, Garberville, Delaware, 7922",
-      "occupation": "business",
-      "company": "QUADEEBO"
-    },
-    {
-      "name": "Ward Manning",
-      "email": "wardmanning@quadeebo.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "561 Overbaugh Place, Umapine, American Samoa, 7567",
-      "occupation": "worker",
-      "company": "INRT"
-    },
-    {
-      "name": "Rosella Mann",
-      "email": "rosellamann@inrt.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "410 Lott Place, Celeryville, Arkansas, 7379",
-      "occupation": "worker",
-      "company": "GEOSTELE"
-    },
-    {
-      "name": "Chelsea Serrano",
-      "email": "chelseaserrano@geostele.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "645 Caton Place, Topanga, Iowa, 6900",
-      "occupation": "business",
-      "company": "EXOTECHNO"
-    },
-    {
-      "name": "Rhonda Frazier",
-      "email": "rhondafrazier@exotechno.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "377 Oxford Street, Florence, New Hampshire, 2368",
-      "occupation": "business",
-      "company": "AQUOAVO"
-    },
-    {
-      "name": "Young Chandler",
-      "email": "youngchandler@aquoavo.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "362 Kingston Avenue, Gambrills, Oklahoma, 1621",
-      "occupation": "worker",
-      "company": "XERONK"
-    },
-    {
-      "name": "Candace Curry",
-      "email": "candacecurry@xeronk.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "403 Lester Court, Vaughn, New Jersey, 6846",
-      "occupation": "worker",
-      "company": "PORTICO"
-    },
-    {
-      "name": "Cornelia Coleman",
-      "email": "corneliacoleman@portico.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "541 Lee Avenue, Kanauga, Maryland, 3510",
-      "occupation": "worker",
-      "company": "MAROPTIC"
-    },
-    {
-      "name": "Pena Yang",
-      "email": "penayang@maroptic.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "619 Navy Walk, Baker, Alabama, 1336",
-      "occupation": "business",
-      "company": "MANGELICA"
-    },
-    {
-      "name": "Hewitt Hinton",
-      "email": "hewitthinton@mangelica.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "506 Canton Court, Glasgow, Virgin Islands, 9052",
-      "occupation": "business",
-      "company": "SCENTRIC"
-    },
-    {
-      "name": "Solis Martin",
-      "email": "solismartin@scentric.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "597 Bethel Loop, Southmont, Virginia, 307",
-      "occupation": "business",
-      "company": "FLOTONIC"
-    },
-    {
-      "name": "Toni Drake",
-      "email": "tonidrake@flotonic.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "295 Sedgwick Place, Sunnyside, Utah, 595",
-      "occupation": "business",
-      "company": "ARCTIQ"
-    },
-    {
-      "name": "Kathie Leach",
-      "email": "kathieleach@arctiq.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "503 Sullivan Place, Soudan, Palau, 1886",
-      "occupation": "worker",
-      "company": "QUONK"
-    },
-    {
-      "name": "Leta Pace",
-      "email": "letapace@quonk.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "145 Creamer Street, Bawcomville, Washington, 8973",
-      "occupation": "worker",
-      "company": "CANDECOR"
-    },
-    {
-      "name": "Latonya Gibson",
-      "email": "latonyagibson@candecor.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "368 Quincy Street, Saranap, South Dakota, 2958",
-      "occupation": "worker",
-      "company": "ZYTRAC"
-    },
-    {
-      "name": "Ella Gallegos",
-      "email": "ellagallegos@zytrac.com",
-      "password": "kjisjakljlksjkl_kljsijir32ieyienfue5n892jbcmze8",
-      "address": "682 Love Lane, Jacumba, Missouri, 6292",
-      "occupation": "business",
-      "company": "ORBALIX"
+const faker = require('faker');
+
+function userFactory(User){
+  function seed(req, res){
+    const counter = req.params.counter;
+
+    Array.prototype.random = function(){
+      return this[Math.floor((Math.random()* this.length))];
     }
-  ]
+    let allUsers = [];
+    for (let i = 0; i < counter; i++){
+      let newUser = new User;
+      newUser.name = faker.name.findName();
+      newUser.email = faker.internet.email();
+      newUser.password = faker.random.word();
+      newUser.address = faker.address.streetAddress();
+      newUser.status = ['verified', 'unverified'].random();
+      newUser.isAdmin = [true, false].random();
+      allUsers.push(newUser);
+    }
+    User.collection.insert(allUsers, function(err, docs){
+      if(err){
+        return console.log(err);
+      } else{
+        res.send('database populated');
+      }
+    });
+
+  }
+
+  return {seed};
+}
+
+module.exports = userFactory
